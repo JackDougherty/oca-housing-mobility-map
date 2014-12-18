83a8228668056769bcb5ee8628cd3fe247415a89
Jack Dougherty <jack.dougherty@trincoll.edu>
2014-12-17 19:47:19 -0500

    Update maps_lib.js

--- js/maps_lib.js
+++ js/maps_lib.js
@@ -116,7 +116,7 @@ var MapsLib = {
     if (loadRadius != "") $("#search_radius").val(loadRadius);
     else $("#search_radius").val(MapsLib.searchRadius);
     //$(":checkbox").prop("checked", "checked");
-    $(":checkbox").not("#kmlLayer").prop("checked", "checked");
+    // $(":checkbox").not("#kmlLayer").prop("checked", "checked");
     $("#result_box").hide();
     
     //-----custom initializers -- default setting to display Polygon1 layer
