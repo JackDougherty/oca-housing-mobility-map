ca26161e68a73131804df69284fc334cafdd6422
Jack Dougherty <jack.dougherty@trincoll.edu>
2014-12-17 19:32:10 -0500

    Update maps_lib.js

--- js/maps_lib.js
+++ js/maps_lib.js
@@ -124,6 +124,7 @@ var MapsLib = {
 	//$("#kmlLayer").attr("checked", "checked"); // KML
     
     $("#rbPolygon1").attr("checked", "checked"); 
+    $(":checkbox").attr("autocomplete", "off");
 	
 	
     
